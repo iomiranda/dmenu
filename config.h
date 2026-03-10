d8379c6f1ed592cf9f5725ab5e65528b97e6f818
iomiranda <miranda.ianpaul@gmail.com>
2026-03-09 20:46:42 -0700

    Added pywal configs

--- config.h
+++ config.h
@@ -7,12 +7,9 @@ static const char *fonts[] = {
 	"JetBrainsMono Nerd Font:size=12"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
-static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeOut] = { "#000000", "#00ffff" },
-};
+
+#include "/home/ian/.cache/wal/colors-wal-dmenu.h"
+
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
 
